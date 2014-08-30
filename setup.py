ade53633ce751ba341f8048d33fd0bb0165a8599
Alexey V Gorshkov <animus@wayround.org>
2014-08-31 03:47:42 +0400

    uploading to PYPi

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 
 import os.path
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='org_wayround_xmpp',
-    version='0.5',
+    version='0.7',
     description='XMPP protocol implimentation',
     author='Alexey V Gorshkov',
     author_email='animus@wayround.org',
-    url='http://wiki.wayround.org/soft/org_wayround_xmpp',
+    url='https://github.com/AnimusPEXUS/org_wayround_xmpp',
     packages=[
         'org.wayround.xmpp'
-        ]
+        ],
+    install_requires = ['org_wayround_utils']
     )
